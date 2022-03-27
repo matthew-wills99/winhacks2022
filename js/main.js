8f9bc7d60e002efdfc9a6dbd2d19b55af91cb1e5
Matthew Wills <matthewwills146@gmail.com>
2022-03-26 23:28:57 -0400

    polishing up home site
    just some stuff

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ function displayModal() {
 
 function hideModal() {
     document.getElementById("loginModal").style.display="none";
+    document.getElementById("email").value="";
+    document.getElementById("pass").value="";
 }
 
 window.onmousedown = function(event) {
     let modal = document.getElementById("loginModal")
     if (event.target == modal) {
-        modal.style.display = "none";
+        hideModal();
     }
 }
